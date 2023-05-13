a6110c608618f711a92c29acf4e66e67337d3fda
Nowy <jaj2232ajn@gmail.com>
2023-05-13 19:14:18 +0600

    🍔UUPDAAATE!🍕

--- src/components/wrapper/header/DropDownMenu/DropDown.jsx
+++ src/components/wrapper/header/DropDownMenu/DropDown.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import scss from './DropDown.module.scss';
+import { NavLink } from "react-router-dom";
 
 const DropDown = () => {
+  const activeLink = ({ isActive }) => {
+    return {
+      border: isActive ? "solid 1px rgb(182, 180, 180)" : "none"
+    };
+  };
+
   return (
     <div className={`${scss.flex} ${scss.flex_col} ${scss.DropDownMenu}`}>
       <ul className={`${scss.flex} ${scss.flex_col} ${scss.gap_4}`}>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-      <li>GitHub</li>
+          <NavLink to="/" className={`${scss.link}`} style={activeLink}>
+            Home
+          </NavLink>
+          <NavLink to="/author" className={`${scss.link}`} style={activeLink}>
+            About
+          </NavLink>
+          <NavLink to="/contacts" className={`${scss.link}`} style={activeLink}>
+            Contact
+          </NavLink>
+          <NavLink
+            to="https://github.com/NowyTeam"
+            className={`${scss.gitlink} ${scss.activeLink}`}>
+            GitHub
+          </NavLink>
       </ul>
     </div>
     
