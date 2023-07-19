9d520d3136757ea47748a1a4c83ee5d644b0753b
NowyTeam <jaj2232ajn@gmail.com>
2023-07-19 15:55:13 +0600

    ContactModalWindow

--- src/components/pages/contacts/Contacts.jsx
+++ src/components/pages/contacts/Contacts.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import scss from "./Conctacts.module.scss";
 import { Helmet } from "react-helmet";
-import Tilt from "react-parallax-tilt";
+import Modal from "react-modal";
+import { CSSTransition } from "react-transition-group";
+// import Tilt from "react-parallax-tilt";
 
 const Contacts = () => {
   const [username, setUsername] = useState("");
@@ -10,6 +12,7 @@ const Contacts = () => {
   const [message, setMessage] = useState("");
   const [headerScroll, setHeaderScroll] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const TOKEN = `6201317143:AAFo6tQ_BVQpE5hK2F5f47mkSesKYQRkeo8`;
   const CHAT_ID = "1814654847";
@@ -21,6 +24,18 @@ const Contacts = () => {
 
     return messageTG;
   };
+
+  useEffect(() => {
+    if (isModalOpen) {
+      const timer = setTimeout(() => {
+        setIsModalOpen(false);
+      }, 5000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [isModalOpen]);
 
   useEffect(() => {
     const changeBackground = () => {
@@ -47,10 +62,11 @@ const Contacts = () => {
         parse_mode: "html",
         text: messageModel(),
       });
-      setAlertMessage(
-        `Сообщение успешно отправлено , ожидайте ответа в течении дня, ${username} !`
-      );
-      setUsername("use");
+      setIsModalOpen(true);
+      // setAlertMessage(
+      //   `Сообщение успешно отправлено , ожидайте ответа в течении дня, ${username} !`
+      // );
+      setUsername("");
       setEmail("");
       setMessage("");
     } catch (error) {
@@ -82,69 +98,78 @@ const Contacts = () => {
             className={`${scss.home_content} ${
               headerScroll ? scss.active : ""
             }`}>
-              <div
-                className={`${scss.gif_image} ${
-                  headerScroll ? scss.active : ""
-                }`}>
-                <h1 className={scss.h1}>Contact Me</h1>
-              </div>
-              <form
-                className={`${scss.form} ${headerScroll ? scss.active : ""}`}
-                onSubmit={handleSubmit}>
-                <label
-                  className={`${scss.label} ${headerScroll ? scss.active : ""}`}
-                  htmlFor="name">
-                  Name
-                  <input
-                    id="name"
-                    className={`${scss.input} ${
-                      headerScroll ? scss.active : ""
-                    }`}
-                    type="text"
-                    placeholder="Nowy"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                  />
-                </label>
-                <label
-                  className={`${scss.label} ${headerScroll ? scss.active : ""}`}
-                  htmlFor="email">
-                  Email
-                  <input
-                    id="email"
-                    className={`${scss.input} ${
-                      headerScroll ? scss.active : ""
-                    }`}
-                    type="email"
-                    placeholder="nowy@example.com"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                </label>
-                <label
-                  className={`${scss.label} ${headerScroll ? scss.active : ""}`}
-                  htmlFor="comment">
-                  Message
-                  <textarea
-                    id="comment"
-                    className={`${scss.message} ${
-                      headerScroll ? scss.active : ""
-                    }`}
-                    value={message}
-                    placeholder="Your Message"
-                    onChange={(e) => setMessage(e.target.value)}
-                    required></textarea>
-                </label>
+            <div
+              className={`${scss.gif_image} ${
+                headerScroll ? scss.active : ""
+              }`}>
+              <h1 className={scss.h1}>Contact Me</h1>
+            </div>
+            <form
+              className={`${scss.form} ${headerScroll ? scss.active : ""}`}
+              onSubmit={handleSubmit}>
+              <label
+                className={`${scss.label} ${headerScroll ? scss.active : ""}`}
+                htmlFor="name">
+                Name
+                <input
+                  id="name"
+                  className={`${scss.input} ${headerScroll ? scss.active : ""}`}
+                  type="text"
+                  placeholder="Nowy"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </label>
+              <label
+                className={`${scss.label} ${headerScroll ? scss.active : ""}`}
+                htmlFor="email">
+                Email
+                <input
+                  id="email"
+                  className={`${scss.input} ${headerScroll ? scss.active : ""}`}
+                  type="email"
+                  placeholder="nowy@example.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </label>
+              <label
+                className={`${scss.label} ${headerScroll ? scss.active : ""}`}
+                htmlFor="comment">
+                Message
+                <textarea
+                  id="comment"
+                  className={`${scss.message} ${
+                    headerScroll ? scss.active : ""
+                  }`}
+                  value={message}
+                  placeholder="Your Message"
+                  onChange={(e) => setMessage(e.target.value)}
+                  required></textarea>
+              </label>
 
-                <button type="submit" className={scss.button}>
-                  Submit
-                </button>
-              </form>
+              <button type="submit" className={scss.button}>
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        contentLabel="Message Sent Modal"
+        className={scss.modal}
+        overlayClassName={scss.modalOverlay}
+      >
+        <h2>Сообщение отправлено!</h2>
+        <p>
+           Спасибо! Ваше сообщение было успешно отправлено. Ожидайте
+          ответа в течение дня.
+        </p>
+      </Modal>
     </>
   );
 };
