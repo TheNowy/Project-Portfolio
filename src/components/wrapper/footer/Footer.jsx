569173fca8798503e111baeac8928387a6ccb192
NowyTeam <jaj2232ajn@gmail.com>
2023-08-05 14:59:17 +0600

    FooterFix

--- src/components/wrapper/footer/Footer.jsx
+++ src/components/wrapper/footer/Footer.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./Footer.scss";
+import scss from "./Footer.module.scss";
 
 export const Footer = () => {
+  const [headerScroll, setHeaderScroll] = useState(false);
+  useEffect(() => {
+    const changeBackground = () => {
+      if (typeof window !== "undefined" && window.scrollY >= 10) {
+        setHeaderScroll(true);
+      } else {
+        setHeaderScroll(false);
+      }
+    };
+
+    changeBackground();
+    window.addEventListener("scroll", changeBackground);
+
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+    };
+  }, []);
   return (
     <div>
       <footer>
-        <div className="footer_cont">
+        <div
+          className={`${scss.footer_cont} ${headerScroll ? scss.active : ""}`}>
           <h1>© 2023 Toktosunov Zhusup</h1>
         </div>
       </footer>
