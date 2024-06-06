1f5bf3a276299698c57ecc924efeb4aa01681e6a
NowyTeam <jaj2232ajn@gmail.com>
2024-06-06 10:44:06 +0600

    Update

--- src/components/pages/error/Error.jsx
+++ src/components/pages/error/Error.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import scss from './Error.module.scss'
+import React from "react";
+import scss from "./Error.module.scss";
+import Svg from "../../../../svg/icon-1.svg";
 
 const Error = () => {
   return (
     <div className={scss.div}>
-      <h1>Error<span>404</span></h1>
+      <h1>
+        Error<span>404</span>
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
